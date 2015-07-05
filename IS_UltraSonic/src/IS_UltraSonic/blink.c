0ec1f372f47a0692a8137e35f157ecdf204ffc74
1sand0s <1sand0s@users.noreply.github.com>
2015-07-06 00:05:17 +0530

    Update blink.c

--- IS_UltraSonic/src/IS_UltraSonic/blink.c
+++ IS_UltraSonic/src/IS_UltraSonic/blink.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<jni.h>
 #include "Ultra_virtual.h"
-JNIEXPORT void JNICALL Java_Ultra_1virtual_blink(JNIEnv *env,jclass c,jstring h)
+/* This is an alternative way of sending data regarding phase delays,
+ * supposing the IOT device cannot handle the processing library*/
+JNIEXPORT void JNICALL Java_Ultra_1virtual_write(JNIEnv *env,jclass c,jstring h)
 {
         const char *ch=(*env)->GetStringUTFChars(env,h,0);
         printf("%s",ch);
         FILE* f;
         f=fopen("/dev/ttyACM0","w");
         fprintf("%s",ch);
-        sleep(10);
-        fprintf("%d",344);
         fclose(f);
         (*env)->ReleaseStringUTFChars(env,h,ch);
         return;
