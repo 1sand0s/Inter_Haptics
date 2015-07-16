6ed2f9402e45071d2fae8888a451fa04d076024c
1sand0s <1sand0s@users.noreply.github.com>
2015-07-16 09:26:47 +0530

    Update write.c

--- IS_UltraSonic/src/IS_UltraSonic/write.c
+++ IS_UltraSonic/src/IS_UltraSonic/write.c
@@ -1,6 +1,17 @@
+/* Copyright (C) 2015 ADITYA T 
+ * ORG: Interactive Spaces
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.*/
+ 
 #include<stdio.h>
 #include<jni.h>
-<<<<<<< HEAD:IS_UltraSonic/src/IS_UltraSonic/write.c
 #include "IS_UltraSonic_Ultra_virtual.h"
 JNIEXPORT void JNICALL Java_Ultra_1virtual_blink(JNIEnv *env,jclass c,jstring h)
 =======
@@ -8,7 +19,6 @@ JNIEXPORT void JNICALL Java_Ultra_1virtual_blink(JNIEnv *env,jclass c,jstring h)
 /* This is an alternative way of sending data regarding phase delays,
  * supposing the IOT device cannot handle the processing library*/
 JNIEXPORT void JNICALL Java_Ultra_1virtual_write(JNIEnv *env,jclass c,jstring h)
->>>>>>> origin/Multi_Threaded_Trial:IS_UltraSonic/src/IS_UltraSonic/blink.c
 {
         const char *ch=(*env)->GetStringUTFChars(env,h,0);
         printf("%s",ch);
