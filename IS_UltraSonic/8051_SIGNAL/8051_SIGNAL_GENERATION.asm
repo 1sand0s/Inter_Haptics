affc12d70e350dbba7423a572b3e998bf8bd1553
1sand0s(Aditya T) <1sand0s@users.noreply.github.com>
2015-07-19 01:59:51 +0530

    Update 8051_SIGNAL_GENERATION.asm

--- IS_UltraSonic/8051_SIGNAL/8051_SIGNAL_GENERATION.asm
+++ IS_UltraSonic/8051_SIGNAL/8051_SIGNAL_GENERATION.asm
@@ -14,17 +14,20 @@ ORG 0000H
 
 LJMP SETTINGS
 
-ORG 0003H
+ORG 0003H ; Vector Address for Harware Interrupt INT0
+;This block ensures to enable serial interrupt so that any MC can communicate with the 8051 serially through UART
+;Since the algorithm uses TH1 for generating the waves and since it is also required for setting the baud rate 
+;for serial communication ,this block stops the wave generation and sets baud rate and hence enables serial communication
 	
-		 CLR TR1
-		 CLR TR0
-		 MOV TL0,#0H
-		 MOV TH0,#0H
-		 MOV TH1,#0FDH
-		 SETB IE.4
-		 RETI
+		 CLR TR1		;Clear Timer 1 run bit
+		 CLR TR0		;Clear Timer 0 run bit
+		 MOV TL0,#0H		;Set TL0 to 0
+		 MOV TH0,#0H		;Set TL1 to 0
+		 MOV TH1,#0FDH		;Set TH1 to FDH to configure baud rate 9600 for serial communication
+		 SETB IE.4		;Enable Serial Interrupt to converse through UART
+		 RETI			;Return
 		 
-ORG 000BH
+ORG 000BH ; Vector Address for Timer 0 Interrupt
 	
 		 CLR A 
 		 MOVC A,@A+DPTR
@@ -37,7 +40,7 @@ ORG 000BH
 		 MOV DPTR,#SINE
 PROC3:	 	 RETI
 
-ORG 0023H
+ORG 0023H ; Vector Address for Serial Interrupt
 		
 		 MOV A,SBUF
 		 CLR RI
@@ -45,16 +48,22 @@ ORG 0023H
 		 INC 60H
 		 RETI
 		 
-SINE: 	 	 db 127,160,191,217,237,250,255,250,237,217,191,160,127,94,63,37,17,4,0,4,17,37,63,94,127
+SINE: 	 	 db 127,160,191,217,237,250,255,250,237,217,191,160,127,94,63,37,17,4,0,4,17,37,63,94,127 ; Define the byte sine values
 
-SETTINGS:	 MOV DPTR,#SINE
-		 MOV R1,#38h
-STORE:		 CLR A
-		 MOVC A,@A+DPTR
-		 INC DPTR
-		 MOV @R1,A
-		 INC R1
-		 CJNE R1,#50h,STORE
+; SETTINGS-	 This is required because since two waves have to been generated and at different frequencies which may or may
+;                not be harmonic therefore bear no correlation between each other, so to prevent interlocking whilst accessing 
+;	     	 the SINE values by DPTR(FOR Timer 0) and R0(FOR Timer 1) we store them in the memory. So DPTR accesses the 
+;                values for Timer0 from code memory and R0 for Timer 1 from RAM address 38H
+
+SETTINGS:	 MOV DPTR,#SINE      ; Store Address of SINE in Data pointer
+		 MOV R1,#38h         ; Move Address 38 to register R1
+STORE:		 CLR A               ; CLear Acc
+		 MOVC A,@A+DPTR      ; Move data at Data pointer(SINE) to ACC
+		 INC DPTR            ; Increment Data pointer to next address
+		 MOV @R1,A           ; Store SINE values into RAM from code memory starting from address 38H
+		 INC R1              ; Increment R1 to point to next address to store next value
+		 CJNE R1,#50h,STORE  ; Continue this until all SINE values have been copied into RAM
+		 
 
 ORG 0080H
 	
