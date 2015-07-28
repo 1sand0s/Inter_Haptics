c02356509f689df86ca100e76d56dc244f80891d
1sand0s <1sand0sardpi@gmail.com>
2015-07-28 13:37:00 +0530

    Arduino_File

---
+++ IS_UltraSonic/Arduino/ultra2.ino
@@ -0,0 +1,58 @@
+#include <comp.h>
+#include <Frequency.h>
+#include <SoftwareSerial.h>
+
+#define INT_PIN 12
+String val;
+FREQUENCY f;
+SoftwareSerial mySerial(10, 11);
+
+void setup()
+{
+  pinMode(INT_PIN,OUTPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  if(Serial.available())
+  {
+    val="";
+    while(Serial.available())
+    {
+      char p=Serial.read();
+      val=val+p;
+    }
+    val=val+" ";
+    Serial.end();
+    Serial.begin(9600);
+    val=val.substring(0,val.indexOf(":")-1);
+    char a[100];
+    val.toCharArray(a,sizeof(a));
+    if(val.startsWith("MOD"))
+    {
+      char *q=a;
+      f=init(0,toFreq(q),MOD);
+      Serial.println(CHECK_FREQ_VALUE(f));
+      mySerial.begin(9600);
+      digitalWrite(INT_PIN,HIGH);
+      delayMicroseconds(1);
+      digitalWrite(INT_PIN,LOW);
+      delayMicroseconds(1);
+      mySerial.write(CHECK_FREQ_VALUE(f));
+     }
+    else if(val.startsWith("CAR"))
+    {
+      char *q=a;
+      f=init(0,toFreq(q),CAR);
+      Serial.println(CHECK_FREQ_VALUE(f));
+      mySerial.begin(9600);
+      digitalWrite(INT_PIN,HIGH);
+      delayMicroseconds(1);
+      digitalWrite(INT_PIN,LOW);
+      delayMicroseconds(1);
+      mySerial.write(CHECK_FREQ_VALUE(f));
+    }
+  }
+  delay(1000);
+}
+   
