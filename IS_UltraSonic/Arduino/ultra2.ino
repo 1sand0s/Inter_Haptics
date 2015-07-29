304485a4423bd86e7fa1f6fbc892758df5944a23
1sand0s(Aditya T) <1sand0s@users.noreply.github.com>
2015-07-29 17:29:45 +0530

    Update ultra2.ino

--- IS_UltraSonic/Arduino/ultra2.ino
+++ IS_UltraSonic/Arduino/ultra2.ino
@@ -28,31 +28,38 @@ void loop()
     val=val.substring(0,val.indexOf(":")-1);
     char a[100];
     val.toCharArray(a,sizeof(a));
-    if(val.startsWith("MOD"))
+    if(val.startsWith("MOD_F"))
     {
       char *q=a;
       f=init(MODULATED_FREQUENCY,toFreq(q),MOD);
       Serial.println(CHECK_FREQ_VALUE(f));
       mySerial.begin(9600);
-      digitalWrite(INT_PIN,HIGH);
-      delayMicroseconds(1);
-      digitalWrite(INT_PIN,LOW);
-      delayMicroseconds(1);
+      Enable_8051_Serial_COM();
       mySerial.write(CHECK_FREQ_VALUE(f));
      }
-    else if(val.startsWith("CAR"))
+    else if(val.startsWith("CAR_F"))
     {
       char *q=a;
       f=init(CARRIER_FREQUENCY,toFreq(q),CAR);
       Serial.println(CHECK_FREQ_VALUE(f));
       mySerial.begin(9600);
+      Enable_8051_Serial_COM();
+      mySerial.write(CHECK_FREQ_VALUE(f));
+    }
+    else if(val.startsWith("CUS_F"))
+    {
+      /* To set custom frequency values*/
+    }
+  }
+  delay(1000);
+}
+void Enable_8051_Serial_COM()
+{
+      /* The Hardware interrupt of the 8051 is made edge-triggered and hence generates an interrupt at the falling edge.
+       * This is done inorder to allow communication with tye 8051 to set the frequencies for signal generation*/
       digitalWrite(INT_PIN,HIGH);
       delayMicroseconds(1);
       digitalWrite(INT_PIN,LOW);
       delayMicroseconds(1);
-      mySerial.write(CHECK_FREQ_VALUE(f));
-    }
-  }
-  delay(1000);
 }
    
