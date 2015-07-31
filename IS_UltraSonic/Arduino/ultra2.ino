0dcafbc14390e60cf7a335cf21f820b0d71defef
1sand0s(Aditya T) <1sand0s@users.noreply.github.com>
2015-07-31 15:08:29 +0530

    Update ultra2.ino

--- IS_UltraSonic/Arduino/ultra2.ino
+++ IS_UltraSonic/Arduino/ultra2.ino
@@ -1,18 +1,19 @@
 #include <comp.h>
 #include <Frequency.h>
 #include <SoftwareSerial.h>
+#include "8051_Signal.h"
 
-#define _INT_PIN 12 // Pin to enable serial com with 8051
-#define _8051_CRYSTAL 24 // In MHz
+
 
 
 String val,val1;
 FREQUENCY f;
 SoftwareSerial mySerial(10, 11);
+Serial_COM8051 com;
 
 void setup()
 {
-  pinMode(_INT_PIN,OUTPUT);
+  pinMode(_8051_SERIAL_ENABLE,OUTPUT);
   Serial.begin(9600);
 }
 void loop()
@@ -37,7 +38,7 @@ void loop()
       f=init(MODULATED_FREQUENCY,toFreq(q),MOD);
       Serial.println(CHECK_FREQ_VALUE(f));
       mySerial.begin(9600);
-      Enable_8051_Serial_COM();
+      com.Enable_8051_Serial_COM();
       mySerial.write(CHECK_FREQ_VALUE(f));
      }
     else if(val.startsWith("CAR_F"))
@@ -46,28 +47,19 @@ void loop()
       f=init(CARRIER_FREQUENCY,toFreq(q),CAR);
       Serial.println(CHECK_FREQ_VALUE(f));
       mySerial.begin(9600);
-      Enable_8051_Serial_COM();
+      com.Enable_8051_Serial_COM();
       mySerial.write(CHECK_FREQ_VALUE(f));
     }
     else if(val.startsWith("CUS_F"))
     {
       /* To set custom frequency values*/
       int freq=(val1.substring(val1.indexOf(":")+1,val1.length()-1)).toInt();
-      int value=(256-(1/(freq*24*(12/(_8051_CRYSTAL * 10^6)))));
+      int value=com.FREQ_TO_HEX(freq);
       mySerial.begin(9600);
-      Enable_8051_Serial_COM();
+      com.Enable_8051_Serial_COM();
       mySerial.write(value);
     }
   }
   delay(1000);
-}
-void Enable_8051_Serial_COM()
-{
-      /* The Hardware interrupt of the 8051 is made edge-triggered and hence generates an interrupt at the falling edge.
-       * This is done inorder to allow communication with tye 8051 to set the frequencies for signal generation*/
-      digitalWrite(_INT_PIN,HIGH);
-      delayMicroseconds(1);
-      digitalWrite(_INT_PIN,LOW);
-      delayMicroseconds(1);
 }
    
